e77ca8118868e8810416715a519101fe8fc44071
Mégane Navarro <mnavarro@live.fr>
2024-01-03 09:39:21 +0100

    Début de l'intégration de la page accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,20 +1,27 @@
 function photographerTemplate(data) {
-    const { name, portrait, city, country, tagline, price } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", "");
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
         const locationP = document.createElement( 'p' );
         locationP.textContent = city + ", " + country;
+        locationP.classList.add("location");
+        locationP.setAttribute("aria-label", "Localisation du photographe " + name);
         const taglineP = document.createElement( 'p' );
         taglineP.textContent = tagline;
+        taglineP.classList.add("tagline");
+        taglineP.setAttribute("aria-label", "Phrase du photographe " + name);
         const priceP = document.createElement( 'p' );
         priceP.textContent = price + "€/jour";
+        priceP.classList.add("price");
+        priceP.setAttribute("aria-label", "Tarif du photographe " + name);
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(locationP);
